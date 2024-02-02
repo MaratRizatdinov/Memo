0ff5d01499c43fdc6220d846c3f6320ea2dc3c96
MaratRizatdinov <maratrizatdinov91@gmail.com>
2024-02-02 17:54:18 +0300

    commit

---
+++ scripts/prepareDate.js
@@ -0,0 +1,15 @@
+//-------------------Обработка даты в требуемый формат------------------
+// import {format} from 'date-fns'
+
+export const prepareDate = (str) => {
+    let options = {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    };
+
+    let date = new Date(str);
+    return date.toLocaleString("ru", options);
+    // let date = new Date(str);
+    // return format(date, "dd.MM.yyyy hh:mm:ss"); // 26.03.2023 10:33:41
+  };
